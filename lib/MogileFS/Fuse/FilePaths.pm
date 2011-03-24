f2464800d66ccacc98d190cca33403cf20831150
Daniel Frett <daniel.frett@ccci.org>
2011-03-24 16:43:12 -0400

    implement directory listing caching to improve performance

--- lib/MogileFS/Fuse/FilePaths.pm
+++ lib/MogileFS/Fuse/FilePaths.pm
@@ -1,6 +1,8 @@
 package MogileFS::Fuse::FilePaths;
 
 use strict;
+use mro 'c3';
+use threads::shared;
 use base qw{MogileFS::Fuse};
 
 our $VERSION = v0.0.1;
@@ -8,8 +10,71 @@ our $VERSION = v0.0.1;
 use Errno qw{EEXIST EIO ENOENT};
 use MogileFS::Client::FilePaths;
 use MogileFS::Fuse::Constants qw{:LEVELS};
+use Params::Validate qw{validate_with BOOLEAN SCALAR};
 
 ##Instance Methods
+
+#method that will initialize the MogileFS::Fuse::FilePaths object
+sub _init {
+	my $self = shift;
+	my %opt = validate_with(
+		'allow_extra' => 1,
+		'params' => \@_,
+		'spec'   => {
+			'filepaths.dircache'          => {'type' => BOOLEAN, 'default' => 1},
+			'filepaths.dircache.duration' => {'type' => SCALAR, 'default' => 2},
+		},
+	);
+
+	#initialize any ancestor classes
+	$self = $self->next::method(%opt);
+
+	#initialize this object
+	$self->{'dirs'} = shared_clone({});
+
+	#return the initialized object
+	return $self;
+}
+
+#method that returns a directory listing for the current directory
+sub _listDir {
+	my $self = shift;
+	my ($path) = @_;
+	$path .= '/' if($path !~ m!/$!so);
+	my $config = $self->_config;
+
+	#short-circuit if the dir cache is disabled
+	return $self->MogileFS->list($path) if(!$config->{'filepaths.dircache'});
+
+	#check to see if the specified path is cached
+	my $cache = $self->{'dirs'};
+	my $dir = $cache->{$path} || {};
+
+	#load the directory listing if the current cached listing is stale
+	if($dir->{'expires'} <= time) {
+		#fetch and store the files in the dir cache
+		$dir = $cache->{$path} = shared_clone({
+			'expires' => time + $config->{'filepaths.dircache.duration'},
+			'files' => [$self->MogileFS->list($path)],
+		});
+	}
+
+	#return the files for the current directory
+	return @{$dir->{'files'}};
+}
+
+#method that flushes the specified dir from the dir cache
+sub _flushDir {
+	my $self = shift;
+	my ($path, $flushParent) = @_;
+	$path .= '/' if($path !~ m!/$!so);
+	delete $self->{'dirs'}->{$path};
+
+	#flush the parent directory from the cache as well
+	$self->_flushDir($1, 1) if($flushParent && $path =~ m!^(.*/)[^/]*/$!so);
+
+	return;
+}
 
 #fetch meta-data about the specified file
 sub get_file_info($) {
@@ -22,16 +87,14 @@ sub get_file_info($) {
 		'is_directory' => 1,
 	} if($path eq '/');
 
-	#process the specified path
-	$path =~ m!^(.*/)([^/]+)$!;
+	#split the path into the directory and the file
+	$path =~ m!^(.*/)([^/]+)$!so;
 	my ($dir, $file) = ($1, $2);
 
-	#look up meta-data for the directory containing the specified file
-	#TODO: maybe cache this lookup
+	#look up meta-data in the directory containing the specified file
 	my $finfo = eval {
 		my $mogc = $self->MogileFS();
-		my @files = $mogc->list($dir);
-		foreach(@files) {
+		foreach($self->_listDir($dir)) {
 			return $_ if($_->{'name'} eq $file);
 		}
 		return undef;
@@ -60,6 +123,23 @@ sub MogileFS {
 }
 
 ##Fuse callbacks
+
+sub fuse_flush {
+	my $self = shift;
+	my ($path, $file) = @_;
+
+	#does the directory cache need a flush after this file is flushed
+	my $needsFlush = eval{$file->writable && $file->dirty};
+
+	#issue actual flush
+	my $resp = $self->next::method(@_);
+
+	#flush the directory cache if necessary
+	eval {$self->_flushDir($file->path, 1)} if($needsFlush);
+
+	#return the response for the flush
+	return $resp;
+}
 
 sub fuse_getattr {
 	my $self = shift;
@@ -107,7 +187,7 @@ sub fuse_getdir {
 	$path = $self->sanitize_path($path);
 
 	#fetch all the files in the specified directory
-	my @files = eval {$self->MogileFS->list($path)};
+	my @files = eval {$self->_listDir($path)};
 	return -EIO() if($@);
 
 	#return this directory listing
@@ -125,6 +205,9 @@ sub fuse_mkdir {
 		my $mogc = $self->MogileFS();
 		die unless($mogc->new_file($file, $self->_config->{'class'})->close);
 		$mogc->delete($file);
+
+		#flush the directory cache
+		$self->_flushDir($path, 1);
 	};
 	if($@) {
 		$self->log(ERROR, 'Error creating new directory: ' . $path);
@@ -132,6 +215,38 @@ sub fuse_mkdir {
 	}
 
 	return 0;
+}
+
+sub fuse_mknod {
+	my $self = shift;
+	my ($path) = @_;
+	$path = $self->sanitize_path($path);
+
+	#issue actual mknod callback
+	my $resp = $self->next::method(@_);
+
+	#flush affected entries from the dir cache
+	eval {$self->_flushDir($path, 1)};
+
+	#return the actual response
+	return $resp;
+}
+
+sub fuse_release {
+	my $self = shift;
+	my ($path, $flags, $file) = @_;
+
+	#does the directory cache need a flush after this file is released
+	my $needsFlush = eval{$file->writable && $file->dirty};
+
+	#issue actual release
+	my $resp = $self->next::method(@_);
+
+	#flush the directory cache if necessary
+	eval {$self->_flushDir($file->path, 1)} if($needsFlush);
+
+	#return the response for the release
+	return $resp;
 }
 
 sub fuse_rename {
@@ -145,7 +260,11 @@ sub fuse_rename {
 
 	#attempt renaming the specified file
 	my $mogc = $self->MogileFS();
-	my $response = eval {$mogc->rename($old, $new)};
+	my $response = eval {
+		$mogc->rename($old, $new);
+		$self->_flushDir($old, 1);
+		$self->_flushDir($new, 1);
+	};
 	if($@ || !$response) {
 		($?, $!) = (-1, '');
 		#set the error code and string if we have a MogileFS::Client object
@@ -159,6 +278,36 @@ sub fuse_rename {
 
 	#return success
 	return 0;
+}
+
+sub fuse_truncate {
+	my $self = shift;
+	my ($path, $size) = @_;
+	$path = $self->sanitize_path($path);
+
+	#issue actual truncate callback
+	my $resp = $self->next::method(@_);
+
+	#flush affected entries from the dir cache
+	eval {$self->_flushDir($path, 1)};
+
+	#return the actual response
+	return $resp;
+}
+
+sub fuse_unlink {
+	my $self = shift;
+	my ($path) = @_;
+	$path = $self->sanitize_path($path);
+
+	#issue actual unlink callback
+	my $resp = $self->next::method(@_);
+
+	#flush affected entries from the dir cache
+	eval {$self->_flushDir($path, 1)};
+
+	#return the actual response
+	return $resp;
 }
 
 1;
